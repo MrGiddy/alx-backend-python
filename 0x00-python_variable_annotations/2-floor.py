0d497953f9c65a4d3513a08a265b916077e815f3
Matumbai Binale <binalematumbai@gmail.com>
2024-08-01 18:50:42 +0300

    (task 2): Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains the definition of `floor` function"""
+from math import floor as flr
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float"""
+    return flr(n)
