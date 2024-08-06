3c97d8e7721f5e5bc66168fc99f1d2cbba7452de
Matumbai Binale <binalematumbai@gmail.com>
2024-08-06 20:49:28 +0300

    (task 0): Use Generator instead of AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,11 +2,11 @@
 """Contains the definiton of `async_generator` coroutine"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Generates a random number between 0 and 10 asynchronously 10 times"""
+async def async_generator() -> Generator[float, None, None]:
+    """Generates a random number sequence between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
