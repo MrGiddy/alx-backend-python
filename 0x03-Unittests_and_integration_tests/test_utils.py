81a3d7bebd6ded416d72a6675e060bef2b9928cb
Matumbai Binale <binalematumbai@gmail.com>
2024-08-26 02:18:26 +0300

    (task 2): Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,12 +2,13 @@
 """Parameterize a unittest"""
 from parameterized import parameterized
 import unittest
+from unittest.mock import Mock, patch
 from typing import (Mapping, Sequence, Any)
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """parameterized unittest for `access_nested_map` function"""
+    """unittest for `access_nested_map` function"""
     @parameterized.expand([
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": 1}, ("a",), 1),
@@ -31,7 +32,38 @@ class TestAccessNestedMap(unittest.TestCase):
             path: Sequence,
             expected: Exception) -> None:
         """test KeyError is raised for given inputs"""
+        # Use assertRaises context manager to catch KeyError
         with self.assertRaises(expected) as context:
             access_nested_map(nested_map, path)
-
+        # Test the exception message is as expected
         self.assertEqual(str(context.exception), repr(path[-1]))
+
+
+class TestGetJson(unittest.TestCase):
+    """unittest for get_json function"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mocked_get):
+        """
+        test get_json returns correctly
+        and that request.get is called correctly
+        """
+        # Create a mock resp. object with
+        # a json method that returns test_payload
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        # Set the mocked get to return mocked response
+        mocked_get.return_value = mock_response
+
+        # call the function and verify the behavior
+        result = get_json(test_url)
+
+        # check that request.get was called once with the correct URL
+        mocked_get.assert_called_once_with(test_url)
+
+        # check that the result of get_json is as expected
+        self.assertEqual(result, test_payload)
