ee91f0268eedb60a356bd432a958446c7bd3a271
Matumbai Binale <binalematumbai@gmail.com>
2024-08-26 18:26:55 +0300

    (task 6): More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -16,9 +16,9 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_org(self, org_name: str, mocked_get_json: MagicMock) -> None:
         """tests the `org` method of GithubOrgClient"""
         mock_response = {
-                'login': 'test-org',
-                'repos_url': 'https://api.github.com/orgs/test-org/repos'
-            }
+            'login': 'test-org',
+            'repos_url': 'https://api.github.com/orgs/test-org/repos'
+        }
         mocked_get_json.return_value = mock_response
 
         test_org = GithubOrgClient(org_name)
@@ -30,7 +30,7 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, mock_response)
 
     def test_public_repos_url(self) -> None:
-        """tests the `public_repos_url` property of GithubOrgClient"""
+        """tests the `_public_repos_url` property of GithubOrgClient"""
         with patch(
                 'client.GithubOrgClient.org', new_callable=PropertyMock
                 ) as mocked_org:
@@ -44,3 +44,28 @@ class TestGithubOrgClient(unittest.TestCase):
 
             expected_url = 'https://api.github.com/orgs/test-org/repos'
             self.assertEqual(result, expected_url)
+
+    @patch('client.get_json')
+    def test_public_repos(self, mocked_get_json):
+        """test the `public_repos` method of GithubOrgClient"""
+        mock_response = [
+            {'id': 123, 'name': 'drumkit-wp'},
+            {'id': 456, 'name': 'car-trumps'},
+            {'id': 789, 'name': 'nfc-talk'}
+        ]
+        mocked_get_json.return_value = mock_response
+
+        with patch(
+                'client.GithubOrgClient._public_repos_url',
+                new_callable=PropertyMock
+                ) as mocked_public_repos_url:
+            mock_url = 'https://api.github.com/orgs/test-org/repos'
+            mocked_public_repos_url.return_value = mock_url
+
+            test_org = GithubOrgClient('test-org')
+            result = test_org.public_repos()
+
+            self.assertEqual(result, ['drumkit-wp', 'car-trumps', 'nfc-talk'])
+            mocked_public_repos_url.assert_called_once()
+
+        mocked_get_json.assert_called_once()
